42e9aca17de1ba5de3fe2e74f5519dde69c1b28d
Claiton Watthier <cla.cgw@gmail.com>
2015-03-26 17:23:32 -0300

    Primeiro commit teste usando o Egit Eclipse http://pt.slideshare.net/loianeg/usando-o-egit-eclipse-plugin-com-o-github

---
+++ Aula003/src/com/google/aula0003/Aula003.java
@@ -0,0 +1,12 @@
+package com.google.aula0003;
+
+public class Aula003 {
+	
+	public static void main(String[] args){
+		System.out.println("Olá 2222");
+	}
+	//pacotes
+	//classes
+	//metodos
+	//váriaveis
+}
