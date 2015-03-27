445376f3d7322bdfbb0cc07ce07b5583a9f2e3c6
Claiton Watthier <cla.cgw@gmail.com>
2015-03-27 08:56:27 -0300

    Aula005

---
+++ Aula005/src/com/google/Aula005/Aula005.java
@@ -0,0 +1,20 @@
+package com.google.Aula005;
+
+public class Aula005 {
+	public static void main (String[] args){
+		
+		int inteiro = 0;
+		double fracao = 0;
+		String t = "texto";
+		boolean bool = true;
+		
+		System.out.println( inteiro );
+		System.out.println( fracao );
+		System.out.println( t );
+		System.out.println( bool );
+		
+		
+	
+	}
+}
+	
