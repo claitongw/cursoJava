c58b9490820c7e4d6e6e6351108821940bb9830c
Claiton Watthier <cla.cgw@gmail.com>
2015-03-27 08:54:41 -0300

    Aula004

---
+++ Aula004/src/com/google/aula004/Aula004.java
@@ -0,0 +1,23 @@
+package com.google.aula004;
+
+public class Aula004 {
+	
+	public static void main(String[] args){
+		
+		int num = 0; //zera a váriavel
+		String s = "aaa";
+		System.out.println( s );
+				
+				/*Caracteristicas das váriaveis
+				 * 1) um nome
+				 * 2) um tipo
+				 * 3) um tamanho
+				 * 4) valor
+				 */
+		
+	}
+	//pacotes
+	//classes
+	//metodos
+	//váriaveis
+}
